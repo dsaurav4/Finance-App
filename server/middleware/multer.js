1b8f312d49c259ce62575e490c39b7b1b41cf2d0
Saurav Dahal <dsaurav2020@gmail.com>
2025-07-09 12:18:10 -0400

    refactor(upload): handle file uploads in memory for production

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,14 +1,6 @@
 import multer from "multer";
 
 // Multer configuration
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    return cb(null, "./public/temp");
-  },
-
-  filename: function (req, file, cb) {
-    return cb(null, file.originalname);
-  },
-});
+const storage = multer.memoryStorage();
 
 export const upload = multer({ storage });
